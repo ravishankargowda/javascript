5965a3791371fe4c1e001af1ccd4043a3781ba1e
ravishankargowda <106077761+ravishankargowda@users.noreply.github.com>
2022-06-09 11:35:10 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,23 @@
+var genral;
+console.log(genral);
+genral=6;
+console.log(typeof genral);
+genral=ravi;
+console.log(typeof genral);
+genral="hi";
+console.log(typeof genral);
+genral=true;
+
+
+console.log(2+3+5);
+console.log(2-3-5);
+console.log("hi" +"this" +"ravi")
+
+
+
+var num1=prompt("enter some value");
+console.log(typeof num1)
+
+console.log(parseInt(num1)+5);
+
+alert("hi this is an alert");
---
+++ operators.js
@@ -0,0 +1,57 @@
+
+//arthametic operators
+/*var diff=5-3;
+console.log("diff:" +diff);
+diff++; // diff=diff+1; diff=diff-1
+
+var product=5*9;
+console.log("product:"+product);	
+
+var sum=9+6+3;
+console.log("sum:"+sum);
+
+var div=9/8;
+console.log("div:"+div);
+
+var mod=8%5;
+console.log("mod:"+mod);*/
+
+//assainment operators
+
+	/*var R=10;
+	R+=10;
+	console.log(R);//r=r+10
+
+var R=10;
+	R-=10;
+	console.log(R);//r=r-10
+
+	var R=10;
+	R*=10;
+	console.log(R);*/
+
+	//comparsion operator
+
+	var s1=9;
+	var s2=10;
+	if (s1==s2) {
+
+		console.log(true);
+	}
+	else
+	{
+
+		console.log(false);
+	}
+
+	var s1=15;
+	var s2=10;
+	if (s1>=s2) {
+
+		console.log(true);
+	}
+	else
+	{
+
+		console.log(false);
+	}
